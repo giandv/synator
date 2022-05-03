f18baac856a883d9852c380dc57c9dc7396f9f9b
Mirko Rizzo <m.rizzo@xtribeapp.com>
2022-05-03 09:49:58 +0200

    add replace-in annotation and did many refinements Xtribe/Web/infrastructure/images/synator#1

--- handlers.py
+++ handlers.py
@@ -8,7 +8,15 @@ import requests
 WATCH_NAMESPACE = os.getenv('WATCH_NAMESPACE', "")
 API_ENDPOINT = os.getenv("API_ENDPOINT", "")
 API_KEY = os.getenv("API_KEY", "")
+PROJECT_ID = os.getenv("PROJECT_ID", "")
 all_namespaces = WATCH_NAMESPACE.split(',')
+
+SYNATOR_WATCH = 'synator/watch'
+SYNATOR_SYNC = 'synator/sync'
+SYNATOR_RELOAD = 'synator/reload'
+SYNATOR_REPLACE_IN = 'synator/replace-in'
+SYNATOR_INCLUDE_NAMESPACE = 'synator/include-namespaces'
+SYNATOR_EXCLUDE_NAMESPACES = 'synator/exclude-namespaces'
 
 
 def watch_namespace(namespace, **_):
@@ -17,24 +25,29 @@ def watch_namespace(namespace, **_):
     return False
 
 
-@kopf.on.create('', 'v1', 'secrets', annotations={'synator/watch': 'yes'}, when=watch_namespace)
-@kopf.on.update('', 'v1', 'secrets', annotations={'synator/watch': 'yes'}, when=watch_namespace)
+@kopf.on.create('', 'v1', 'secrets', annotations={SYNATOR_WATCH: 'yes'}, when=watch_namespace)
+@kopf.on.update('', 'v1', 'secrets', annotations={SYNATOR_WATCH: 'yes'}, when=watch_namespace)
 def update_secret_manager(body, meta, spec, status, old, new, diff, **kwargs):
     api = kubernetes.client.CoreV1Api()
     secret = api.read_namespaced_secret(meta.name, meta.namespace)
-    sec_data = secret.data
-    value = base64.b64decode(sec_data.strip().split()[1].translate(None, '}\''))
-    my_headers = {'Api-Key': f'Bearer {API_KEY}', 'Content-Type': 'application/json'}
-    my_body = {'name': secret.metadata.name, 'value': value, 'projectId': 'xtribeapp-1279'}
-    try:
-        response = requests.post(API_ENDPOINT + "/api/internal/secret/add-version", data=my_body, headers=my_headers)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as error:
-        print(error)
+    if SYNATOR_REPLACE_IN in secret.metadata.annotations:
+        name_secret_manager = secret.metadata.annotations[SYNATOR_REPLACE_IN]
+        sec_data = secret.data
+        value = base64.b64decode(sec_data.strip().split()[1].translate(None, '}\''))
+        my_headers = {'Api-Key': f'Bearer {API_KEY}', 'Content-Type': 'application/json'}
+        my_body = {'name': name_secret_manager, 'value': value, 'projectId': PROJECT_ID}
+        try:
+            response = requests.post(API_ENDPOINT + "/api/internal/secret/add-version", data=my_body,
+                                     headers=my_headers)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            print(error)
+    else:
+        print('Error: There is no reference of the secret to be updated')
 
 
-@kopf.on.create('', 'v1', 'secrets', annotations={'synator/sync': 'yes'}, when=watch_namespace)
-@kopf.on.update('', 'v1', 'secrets', annotations={'synator/sync': 'yes'}, when=watch_namespace)
+@kopf.on.create('', 'v1', 'secrets', annotations={SYNATOR_SYNC: 'yes'}, when=watch_namespace)
+@kopf.on.update('', 'v1', 'secrets', annotations={SYNATOR_SYNC: 'yes'}, when=watch_namespace)
 def update_secret(body, meta, spec, status, old, new, diff, **kwargs):
     api = kubernetes.client.CoreV1Api()
     namespace_response = api.list_namespace()
@@ -42,7 +55,7 @@ def update_secret(body, meta, spec, status, old, new, diff, **kwargs):
     namespaces.remove(meta.namespace)
 
     secret = api.read_namespaced_secret(meta.name, meta.namespace)
-    secret.metadata.annotations.pop('synator/sync')
+    secret.metadata.annotations.pop(SYNATOR_SYNC)
     secret.metadata.resource_version = None
     secret.metadata.uid = None
     for ns in parse_target_namespaces(meta, namespaces):
@@ -56,8 +69,8 @@ def update_secret(body, meta, spec, status, old, new, diff, **kwargs):
             api.create_namespaced_secret(ns, secret)
 
 
-@kopf.on.create('', 'v1', 'configmaps', annotations={'synator/sync': 'yes'}, when=watch_namespace)
-@kopf.on.update('', 'v1', 'configmaps', annotations={'synator/sync': 'yes'}, when=watch_namespace)
+@kopf.on.create('', 'v1', 'configmaps', annotations={SYNATOR_SYNC: 'yes'}, when=watch_namespace)
+@kopf.on.update('', 'v1', 'configmaps', annotations={SYNATOR_SYNC: 'yes'}, when=watch_namespace)
 def update_config_map(body, meta, spec, status, old, new, diff, **kwargs):
     api = kubernetes.client.CoreV1Api()
     namespace_response = api.list_namespace()
@@ -65,7 +78,7 @@ def update_config_map(body, meta, spec, status, old, new, diff, **kwargs):
     namespaces.remove(meta.namespace)
 
     cfg = api.read_namespaced_config_map(meta.name, meta.namespace)
-    cfg.metadata.annotations.pop('synator/sync')
+    cfg.metadata.annotations.pop(SYNATOR_SYNC)
     cfg.metadata.resource_version = None
     cfg.metadata.uid = None
     for ns in parse_target_namespaces(meta, namespaces):
@@ -82,8 +95,8 @@ def update_config_map(body, meta, spec, status, old, new, diff, **kwargs):
 def parse_target_namespaces(meta, namespaces):
     namespace_list = []
     # look for a namespace inclusion label first, if we don't find that, assume all namespaces are the target
-    if 'synator/include-namespaces' in meta.annotations:
-        value = meta.annotations['synator/include-namespaces']
+    if SYNATOR_INCLUDE_NAMESPACE in meta.annotations:
+        value = meta.annotations[SYNATOR_INCLUDE_NAMESPACE]
         namespaces_to_include = value.replace(' ', '').split(',')
         for ns in namespaces_to_include:
             if ns in namespaces:
@@ -94,8 +107,8 @@ def parse_target_namespaces(meta, namespaces):
     else:
         # we didn't find a namespace inclusion label, so let's see if we were told to exclude any
         namespace_list = namespaces
-        if 'synator/exclude-namespaces' in meta.annotations:
-            value = meta.annotations['synator/exclude-namespaces']
+        if SYNATOR_EXCLUDE_NAMESPACES in meta.annotations:
+            value = meta.annotations[SYNATOR_EXCLUDE_NAMESPACES]
             namespaces_to_exclude = value.replace(' ', '').split(',')
             if len(namespaces_to_exclude) < 1:
                 print(
@@ -120,8 +133,8 @@ def new_namespace(spec, name, meta, logger, **kwargs):
         # TODO: Add configmap
         for secret in api_response.items:
             # Check secret have annotation
-            if secret.metadata.annotations and secret.metadata.annotations.get("synator/sync") == "yes":
-                secret.metadata.annotations.pop('synator/sync')
+            if secret.metadata.annotations and secret.metadata.annotations.get(SYNATOR_SYNC) == "yes":
+                secret.metadata.annotations.pop(SYNATOR_SYNC)
                 secret.metadata.resource_version = None
                 secret.metadata.uid = None
                 for ns in parse_target_namespaces(secret.metadata, [name]):
@@ -149,8 +162,8 @@ def reload_pod_config(body, meta, spec, status, old, new, diff, **kwargs):
     print(ns, meta.name)
     for pod in pods.items:
         # Find which pods use this secrets
-        if pod.metadata.annotations and pod.metadata.annotations.get('synator/reload'):
-            if any('configmap:' + meta.name in s for s in pod.metadata.annotations.get('synator/reload').split(',')):
+        if pod.metadata.annotations and pod.metadata.annotations.get(SYNATOR_RELOAD):
+            if any('configmap:' + meta.name in s for s in pod.metadata.annotations.get(SYNATOR_RELOAD).split(',')):
                 # Reload pod
                 api.delete_namespaced_pod(
                     pod.metadata.name, pod.metadata.namespace)
@@ -165,8 +178,8 @@ def reload_pod_secret(body, meta, spec, status, old, new, diff, **kwargs):
     print(ns, meta.name)
     for pod in pods.items:
         # Find which pods use this secrets
-        if pod.metadata.annotations and pod.metadata.annotations.get('synator/reload'):
-            if any('secret:' + meta.name in s for s in pod.metadata.annotations.get('synator/reload').split(',')):
+        if pod.metadata.annotations and pod.metadata.annotations.get(SYNATOR_RELOAD):
+            if any('secret:' + meta.name in s for s in pod.metadata.annotations.get(SYNATOR_RELOAD).split(',')):
                 # Reload pod
                 api.delete_namespaced_pod(
                     pod.metadata.name, pod.metadata.namespace)
