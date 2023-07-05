7f5a515f9458707d22a6043039feb9092d91006e
Gianluca Di Vincenzo <divincenzogianluca@gmail.com>
2023-07-05 10:25:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
+
 case "${GITHUB_EVENT_NAME}" in
     push)
         git push -f --all target
